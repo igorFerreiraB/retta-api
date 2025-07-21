524b486be905ec1b55e448332c9b05d22b1e9036
iggomes <igorfgb2006@gmail.com>
2025-07-21 19:42:12 -0300

    featjob): adiciona campos faltantes em orgão

--- src/app/Jobs/OrgaosJob.php
+++ src/app/Jobs/OrgaosJob.php
@@ -20,15 +20,20 @@ class OrgaosJob implements ShouldQueue
     public function handle(): void
     {
         $service = new \App\Services\DeputadoService();
-        $dados = $service->getEventos($this->deputadoId);
+        $dados = $service->getEventos($this->deputadoApiId);
 
         foreach ($dados['dados'] ?? [] as $item) {
             \App\Models\Orgao::updateOrCreate(
                 ['id_orgao' => $item['idOrgao']],
                 [
-                    'deputado_api_id' => $this->deputadoId,
+                    'deputado_api_id' => $this->deputadoApiId,
+                    'id_orgao' => $item['idOrgao'],
+                    'uri_orgao' => $item['uriOrgao'],
                     'sigla_orgao' => $item['siglaOrgao'],
                     'nome_orgao' => $item['nomeOrgao'],
+                    'nome_publicacao' => $item['nomePublicacao'],
+                    'titulo' => $item['titulo'],
+                    'cod_titulo' => $item['codTitulo'],
                     'data_inicio' => $item['dataInicio'],
                     'data_fim' => $item['dataFim'],
                     'dados_completos' => $item,
