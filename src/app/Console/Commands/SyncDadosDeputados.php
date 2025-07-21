3660360c806560466ad91d638b0d006ec413a448
iggomes <igorfgb2006@gmail.com>
2025-07-21 17:52:47 -0300

    feat(command): adiciona comando para importar deputados para sincronia dos dados

--- src/app/Console/Commands/SyncDadosDeputados.php
+++ src/app/Console/Commands/SyncDadosDeputados.php
@@ -11,20 +11,47 @@ class SyncDadosDeputados extends Command
      *
      * @var string
      */
-    protected $signature = 'app:sync-dados-deputados';
+    protected $signature = 'sync:dados-deputados';
 
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'Comando para importar os deputados e sincronizar os dados';
 
     /**
      * Execute the console command.
      */
     public function handle()
     {
-        //
+        $service = new \App\Services\DeputadoService();
+        $response = $service->getDeputados();
+
+        foreach ($response['dados'] ?? [] as $item) {
+            \App\Models\Deputado::updateOrCreate(
+                ['api_id' => $item['id']],
+                [
+                    'nome' => $item['nome'],
+                    'sigla_partido' => $item['siglaPartido'] ?? null,
+                    'sigla_uf' => $item['siglaUf'] ?? null,
+                    'email' => $item['email'] ?? null,
+                    'uri_foto' => $item['urlFoto'] ?? null,
+                    'id_legislatura' => $item['idLegislatura'] ?? null,
+                    'dados_completos' => $item,
+                    'ultima_atualizacao' => now(),
+                ]
+            );
+        }
+
+        $deputados = \App\Models\Deputado::pluck('api_id');
+
+        foreach ($deputados as $id) {
+
+            \App\Jobs\DespesasJob::dispatch($id);
+            \App\Jobs\OrgaosJob::dispatch($id);
+        }
+
+        $this->info("Jobs de sincronização despachados!");
     }
 }
